9501d2110c5b58965e61eb39353a07918f937a98
SauravMahalik <80640726+SauravMahalik@users.noreply.github.com>
2021-08-31 02:04:18 +0530

    Add files via upload

---
+++ class1/p1.py
@@ -0,0 +1,9 @@
+x = 5
+y = 7
+print ("Before swap: ")
+print("Value of x : ", x, " and y : ", y)
+
+x, y = y, x
+
+print ("After swap: ")
+print("Value of x : ", x, " and y : ", y)
---
+++ class1/p2.py
@@ -0,0 +1,4 @@
+import math
+[x1,x2,y1,y2] = [1,4,2,6]
+distance = math.sqrt( math.pow(x2-x1, 2) + math.pow(y1-y1, 2))	
+print(distance)
